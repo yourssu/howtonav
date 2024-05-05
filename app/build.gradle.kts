90d8e8de0e43c3b7ddb22960b27cd5612667be30
cometj03 <harryjung1001@gmail.com>
2024-05-05 15:02:31 +0900

    의존성 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,23 +56,14 @@ dependencies {
     implementation(project(":feature:setting"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-
-    implementation(libs.compose.navigation)
-    implementation(libs.compose.hilt.navigation)
-    implementation(libs.androidx.navigation.fragment.ktx)
-
-    implementation(libs.androidx.navigation.fragment.compose) // 이 의존성이 있어야 composable 태그 사용 가능
-
-
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment.compose) // 이 의존성이 있어야 composable 태그 사용 가능
+
     implementation(libs.androidx.activity)
 
     testImplementation(libs.junit)
