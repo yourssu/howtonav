f9c88c79b7d3ba6be2336a019d14d996a4547c0e
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-06-18 20:22:38 +0900

    feat: implement deeplink (#6)

--- feature/setting/src/main/java/com/yourssu/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/yourssu/setting/SettingScreen.kt
@@ -1,19 +1,20 @@
 package com.yourssu.setting
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SettingScreen(modifier: Modifier = Modifier) {
-    Column {
-        
+    Column(modifier) {
+        Text("Setting")
     }
 }
 
 @Preview
 @Composable
 private fun SettingScreenPreview() {
-
+    SettingScreen()
 }
